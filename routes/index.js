5446372d743a5008e753431cd8f14cba63d7627b
ddy250 <odedsagi@gmail.com>
2018-05-23 11:30:21 +0300

    heroku api 10 events

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ var router = express.Router();
 var request = require('request');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-request('https://status.heroku.com/api/v3/issues?since=2012-04-24&limit=1',  function (error, response, body) {
-    res.render('index', {
-        title : 'oded',
-        title2: JSON.stringify(body)}
- );
+    request("https://status.heroku.com/api/v3/issues?since=2012-04-24&limit=10", function (error, response, body) {
+        var c=JSON.parse(body);
+        var length = Object.keys(c[0]).length;
+        console. log (c[0]);
+        res.render('index', {
+            title: 'Heroku status as of today',
+            c: c,
+            length : length
+        });
 
-});
+    });
 });
 
 module.exports = router;
